27d553026a3398e037264115e8122efb5af6f1ea
Batool Kazmi <kazmib6@gmail.com>
2025-02-06 14:42:27 -0500

    offer section draft done

--- src/components/sections/Offer.tsx
+++ src/components/sections/Offer.tsx
@@ -1,15 +1,35 @@
+import Logo from "../../assets/Logo.svg";
+
 const OfferSection = () => {
   return (
     <div
       id="offer"
-      className="offer-section bg-[#004aad] flex flex-col items-center pt-[3rem] sm:flex-none sm:pb-4"
+      className="offer-section w-full grid grid-cols-1 sm:grid-cols-3 sm:grid-rows-2 text-center"
     >
-      <div className="w-[50%] h-full text-center">
-        <h3>What We Offer</h3>
-        <p>Trent University’s first-of-its-kind finance case competition</p>
-        <p>Bringing Together 100+ Students</p>
-        <p>20+ Industry Professionals</p>
-        <p>Free In-Person Event</p>
+      {/* Row 1 */}
+      <div className="h-[17em] bg-[#004aad] text-white font-semibold flex items-center justify-center">
+        What We Offer
+      </div>
+      <div className="h-[17em] bg-white text-[#004aad] font-semibold flex items-center justify-center">
+        Trent University’s first-of-its-kind finance case competition
+      </div>
+      <div className="h-[17em] bg-[#004aad] text-white font-semibold flex items-center justify-center">
+        Bringing Together 100+ Students
+      </div>
+
+      {/* Row 2 */}
+      <div className="h-[17em] bg-white text-[#004aad] font-semibold flex items-center justify-center">
+        20+ Industry Professionals
+      </div>
+      <div className="h-[17em] bg-[#004aad] flex items-center justify-center">
+        <img
+          src={Logo}
+          alt="Buy the River Whale Logo"
+          className="h-10 w-auto em:h-16"
+        />
+      </div>
+      <div className="h-[17em] bg-white text-[#004aad] font-semibold flex items-center justify-center">
+        Free In-Person Event & Exciting Prizes
       </div>
     </div>
   );
