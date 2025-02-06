6381ff87b569cbdeed1280e78bfe78e0d611f0d3
Batool Kazmi <kazmib6@gmail.com>
2025-02-06 13:13:26 -0500

    created draft for about section

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -2,19 +2,40 @@ const AboutSection = () => {
   return (
     <div
       id="about"
-      className="about-section bg-white sm:h-[40vh] text-black flex flex-col justify-center sm:flex-row p-6"
+      className="about-section !my-5 bg-white text-[#004aad] flex flex-col justify-center sm:flex-row p-6"
     >
-      <div className="h-[50%] pt-4 sm:h-[100%] text-center flex flex-col items-center justify-center text-4xl sm:text-6xl font-semibold px-2">
+      <div className="text-[#004aad] h-[30% !py-3 sm:!px-10 pt-4 sm:h-[100%] text-left flex flex-col  justify-center !text-4xl sm:text-6xl font-semibold px-2">
         <h3>About</h3>
         <h3>Us</h3>
       </div>
-      <div className="h-[50%] flex flex-col sm:justify-center text-center pb-8 sm:w-[60%] text-xl px-6">
-        <h3 className="text-4xl font-semibold">Our Story</h3>
-        <p className="pt-4 leading-6">
-          Buy the River is Trent University’s first-of-its-kind finance case
-          competition, bringing together 100+ students and 20+ industry
-          professionals.
-        </p>
+      <div className="text-[#004aad] h-[70%] flex flex-col sm:justify-center text-left pb-8 sm:w-[60%] text-xl px-6">
+        <div className="!my-2">
+          <h3 className="!my-4 !text-2xl font-semibold">Our Story</h3>
+          <p className="pt-4 leading-6">
+            Buy the River is Trent University’s first-of-its-kind finance case
+            competition, bringing together 100+ students and 20+ industry
+            professionals. This event marks the beginning of what promises to be
+            a lasting legacy in finance competitions at Trent. It is a
+            collaborative effort between the Trent Business Students'
+            Association (TBSA) and the Trent Finance and Investment Club (TFIC).
+            ​ The one-day event, scheduled for March 1st, features dynamic pitch
+            presentations, immersive stock simulations, and a series of
+            interactive experiences, including career workshops, networking
+            opportunities, and engaging trivia competitions.
+          </p>
+        </div>
+        <div className="!my-2">
+          <h3 className="!my-4 !text-2xl font-semibold">Who We Are</h3>
+          <p className="pt-4 leading-6">
+            Buy the River is organized by the same team behind HackTrent,
+            Trent’s largest hackathon, and the ‘Building Bridges’ Business Gala,
+            university’s biggest networking event. With the support of college
+            principals, esteemed mentors, and leading faculty including the
+            Chair of Business and Humanities, this event is poised to elevate
+            the student experience and foster meaningful connections within the
+            business community
+          </p>
+        </div>
       </div>
     </div>
   );
