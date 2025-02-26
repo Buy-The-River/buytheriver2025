e3a63eeda12649416d6a59f52ffd16c4c5e491c6
Batool Kazmi <kazmib6@gmail.com>
2025-02-26 18:05:22 -0500

    created new update

--- src/components/sections/Case.tsx
+++ src/components/sections/Case.tsx
@@ -1,8 +1,7 @@
 import { useState, useEffect } from "react";
 import CaseRelease from "../../assets/BuyTheRiver2025_case_release.pdf";
 import { Button } from "../../components/ui/button"; // Ensure you are using the correct UI component for buttons
-import { FaFilePdf } from "react-icons/fa6";
-import { FaFileExcel } from "react-icons/fa6";
+import { FaFilePdf, FaFileExcel } from "react-icons/fa6";
 
 const CaseSection = () => {
   const [isMobile, setIsMobile] = useState(false);
@@ -28,10 +27,12 @@ const CaseSection = () => {
   return (
     <div
       id="case"
-      className="sponsor-section min-h-[50vh] text-black !text-white !bg-[#004aad] !leading-18
-                 !p-20 text-center !text-6xl transition-colors duration-300"
+      className="sponsor-section min-h-[50vh] text-black !text-white !bg-[#004aad] !leading-18 
+                 !p-20 text-center flex flex-col items-center justify-center transition-colors duration-300"
     >
-      <h3 className="!m-2 text-lg font-semibold md:col-span-3">The Case</h3>
+      <h3 className="!my-6 !text-4xl md:!text-6xl font-semibold text-center">
+        The Case
+      </h3>
 
       {/* PDF Toggle Button - Only Visible on Desktop */}
       {!isMobile && (
@@ -39,7 +40,8 @@ const CaseSection = () => {
           <Button
             size="lg"
             variant="solid"
-            className="!bg-[#FFDE59] !text-[#004aad] !px-6 !py-3 lg:py-10! !m-5 text-center text-base sm:text-lg! md:text-xl! lg:text-2xl! xl:text-3xl! 2xl:text-4xl! font-semibold border-0 rounded-lg transition-all duration-300 hover:bg-white! !text-sm"
+            className="!bg-[#FFDE59] !text-[#004aad] !px-6 !py-3 !m-5 text-center 
+                       text-base font-semibold border-0 rounded-lg transition-all duration-300 hover:bg-white"
             onClick={togglePdf}
           >
             {showPdf ? "Hide PDF" : "Show PDF"}
@@ -49,47 +51,61 @@ const CaseSection = () => {
 
       {/* PDF Embed - Only Visible on Desktop */}
       {!isMobile && showPdf && (
-        <div className="flex justify-center mt-6">
+        <div className="w-full flex justify-center mt-6">
           <iframe
             src={CaseRelease}
-            width="80%"
+            width="100%"
             height="600px"
             title="PDF Viewer"
-            className="shadow-lg rounded-lg"
+            className="shadow-lg rounded-lg max-w-screen-lg"
           ></iframe>
         </div>
       )}
 
-      {/* Always Visible Download Buttons (Side by Side on Desktop, Stacked on Mobile) */}
-      <div
-        className={`mt-6 flex flex-col ${
-          isMobile ? "gap-2" : "md:grid md:grid-cols-2 md:gap-4"
-        }`}
-      >
+      {/* Always Visible Download Buttons */}
+      <div className="md:!mt-6 flex flex-col md:flex-row md:gap-6 items-center">
         {/* PDF Download Button */}
-        <a href={CaseRelease} download>
+        <a href={CaseRelease} download className="m-3">
           <Button
             size="lg"
             variant="solid"
-            className="!bg-[#FFDE59] !text-[#004aad] !px-6 !py-3 lg:py-10! !m-5 text-center text-base sm:text-lg! md:text-xl! lg:text-2xl! xl:text-3xl! 2xl:text-4xl! font-semibold border-0 rounded-lg transition-all duration-300 hover:bg-white! !text-sm"
+            className="!bg-[#FFDE59] !text-[#004aad] !px-6 !py-3 text-center 
+                       text-base font-semibold border-0 rounded-lg transition-all duration-300 hover:bg-white"
           >
-            <FaFilePdf />
+            <FaFilePdf className="mr-2" />
             Download Case PDF
           </Button>
         </a>
 
         {/* Excel Download Button */}
-        <a href="/case_figures_BTR2025.xlsx" download>
+        <a href="/case_figures_BTR2025.xlsx" download className="m-3">
           <Button
             size="lg"
             variant="solid"
-            className="!bg-[#FFDE59] !text-[#004aad] !px-6 !py-3 lg:py-10! !m-5 text-center text-base sm:text-lg! md:text-xl! lg:text-2xl! xl:text-3xl! 2xl:text-4xl! font-semibold border-0 rounded-lg transition-all duration-300 hover:bg-white! !text-sm"
+            className="!bg-[#FFDE59] !text-[#004aad] !px-6 !py-3 text-center 
+                       text-base font-semibold border-0 rounded-lg transition-all duration-300 hover:bg-white"
           >
-            <FaFileExcel />
+            <FaFileExcel className="mr-2" />
             Download Excel File
           </Button>
         </a>
       </div>
+
+      <h3 className="!my-6 !text-4xl md:!text-6xl font-semibold text-center">
+        Guides, Hints & Rubric
+      </h3>
+
+      <a href="/Guides, Hints & The Rubric.xlsx" download className="m-3">
+        <Button
+          size="lg"
+          variant="solid"
+          className="!bg-[#FFDE59] !text-[#004aad] !px-6 !py-3 text-center 
+                     text-base font-semibold border-0 rounded-lg transition-all duration-300 hover:bg-white"
+        >
+          <FaFileExcel className="mr-2" />
+          Download Excel File
+        </Button>
+      </a>
     </div>
   );
 };
--- src/components/sections/Sponsors.tsx
+++ src/components/sections/Sponsors.tsx
@@ -3,10 +3,6 @@ import Chatime from "../../../src/assets/Chatime-Logo.png";
 import Osmows from "../../../src/assets/osmows-mobile-retina-logo.webp";
 import Pj from "../../../src/assets/PJ Logo Colour.png";
 import Business from "../../../src/assets/SchoolOfBusiness.png";
-import Champlain from "../../../src/assets/2023 Champlain College Crest - No Words.png";
-import LadyEaton from "../../../src/assets/lady_eaton_logo.png";
-import Trail from "../../../src/assets/Trail.png";
-import Otonabee from "../../../src/assets/OtonabeeCollegeCrest.png";
 import Gzowski from "../../../src/assets/gzowski_crest.png";
 
 const SponsorsSection = () => {
@@ -25,7 +21,7 @@ const SponsorsSection = () => {
         });
       },
       {
-        threshold: 0.5,
+        threshold: 0.2,
       }
     );
 
@@ -45,73 +41,46 @@ const SponsorsSection = () => {
       id="sponsors"
       ref={sponsorsRef}
       className={`sponsor-section min-h-[50vh] text-black !text-[#004aad] !leading-18
-                 !border-t-10 !border-[#004aad] !p-30 text-center !text-6xl transition-colors duration-300 ${whiteBgClass}`}
+                 !border-t-10 !border-[#004aad] !p-10 text-center !text-6xl transition-colors duration-300 ${whiteBgClass}`}
     >
-      <h2 className="!text-6xl !mb-7 text-3xl font-bold">Our Sponsors</h2>
+      <h2 className="sm:!text-6xl !mb-7 !text-4xl font-bold">Our Sponsors</h2>
 
       {/* Centered Sponsors Section */}
-      <div className="flex !m-10 flex-wrap justify-center items-center gap-8">
+      <div className="flex md:!m-5 flex-wrap justify-center items-center">
         <div className="flex justify-center items-center">
           <img
             src={Chatime}
             alt="Chatime Logo"
-            className="!h-30 !w-auto object-contain"
+            className="!h-50 !w-auto object-contain"
           />
         </div>
         <div className="flex justify-center items-center">
           <img
             src={Osmows}
             alt="Osmow's Logo"
-            className="!h-30 !w-auto object-contain"
+            className="!h-50 !w-auto object-contain"
           />
         </div>
         <div className="flex justify-center items-center">
           <img
             src={Pj}
             alt="Pitch Jams Logo"
-            className="!h-30 !w-auto object-contain"
+            className="!h-50 !w-auto object-contain"
           />
         </div>
-        <div className="flex justify-center items-center">
-          <img
-            src={Champlain}
-            alt="Champlain College Logo"
-            className="!h-30 w-auto object-contain"
-          />
-        </div>
-        <div className="flex justify-center items-center">
-          <img
-            src={LadyEaton}
-            alt="Lady Eaton College Logo"
-            className="!h-30 w-auto object-contain"
-          />
-        </div>
-        <div className="flex justify-center items-center">
-          <img
-            src={Trail}
-            alt="Trail College Logo"
-            className="!h-30 w-auto object-contain"
-          />
-        </div>
-        <div className="flex justify-center items-center">
-          <img
-            src={Otonabee}
-            alt="Otonabee College Logo"
-            className="!h-30 w-auto object-contain"
-          />
-        </div>
+
         <div className="flex justify-center items-center">
           <img
             src={Gzowski}
             alt="Gzowski College Logo"
-            className="!h-30 w-auto object-contain"
+            className="!h-50 w-auto  object-contain"
           />
         </div>
         <div className="flex justify-center items-center">
           <img
             src={Business}
             alt="School of Business Logo"
-            className="h-24 w-auto object-contain"
+            className="!h-30 w-auto object-contain"
           />
         </div>
       </div>
